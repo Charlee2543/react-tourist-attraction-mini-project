2442a66d1d0255cfa50842765fea595f72d6f3a5
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-03-28 23:03:36 +0700

    feat:set arrayobject data to show in web

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,10 +18,10 @@ import { useState, useEffect } from 'react';
 fetch(`https://demo.dataverse.org/api/search?q=${value}`)
 */
 function App() {
-    const [data, Setdata] = useState([]);
+    const [dataServer, Setdata] = useState([]);
     const [inputSearch, setInputSearch] = useState('');
 
-    const getData = debounce(async (keywords) => {
+    const getData = async (keywords) => {
         try {
             const result = await axios.get(
                 `http://localhost:4001/trips?keywords=${
@@ -30,20 +30,23 @@ function App() {
             );
             console.log('result: ', result.data.data);
             Setdata(result.data.data);
-            console.log('getData complete ');
+            console.log('getData complete');
         } catch (error) {
             console.log('error getData: ', error);
         }
-    }, 1500);
+    };
 
     useEffect(() => {
         getData();
     }, []);
-    useEffect(() => {
-        getData(inputSearch);
-    }, [inputSearch]);
+    useEffect(
+        debounce(() => {
+            getData(inputSearch);
+        }, 1500),
+        [inputSearch]
+    );
     console.log('inputSearch: ', inputSearch);
-    console.log('data: ', data);
+    console.log('data: ', dataServer);
 
     // const text =
     //     'เริ่มเที่ยวเชิงอนุรักษ์แบบชิลล์ๆ กันที่แรก ที่ศูนย์อนุรักษ์ป่าชายเลน คลองโคน ด้วยกิจกรรมอัดแน่น ทั้งการล่องเรือชมป่าชายเลน รวมสร้างแหล่งอาหารให้สัตว์ทะเลด้วยการปลูกต้นแสม ต้นลำพู สัมผัสวิถีชีวิตชาวประมงในการเลี้ยงหอยนางรม และหอยแมลงภู่ แถมด้วยไฮไลท์น่ารักๆ นั่นก็คือ เจ้าลิงแสมที่ออกมาวิ่งเล่น ต้อนรับนักท่องเที่ยวชมความสมบูรณ์ของระบบนิเวศน์ที่นี่';
@@ -65,58 +68,70 @@ function App() {
                     onChange={(event) => setInputSearch(event.target.value)}
                 />
             </label>
-            <section className="max-w-7xl w-[90%] mx-auto m-6 space-y-6 overflow-hidden">
-                <div className="p-4 bg-white rounded-lg shadow-md flex gap-4 ">
-                    <img
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png"
-                        alt=""
-                        className="w-1/3 object-cover object-center rounded-3xl"
-                    />
-                    <div className="flex-1 overflow-hidden ">
-                        <h2 className="text-xl font-semibold  ">
-                            คู่มือเที่ยวเกาะช้าง กิน เที่ยว พักที่ไหนดี?
-                            อ่านจบครบที่เดียว!
-                        </h2>
-                        <p className={`text-lg  mt-1 w-full text-gray-600  `}>
-                            {/* {text.length > 100
-                                ? text.slice(0, 100) + '...'
-                                : text} */}
-                            {/* {text} */}
-                        </p>
-                        <a
-                            href=""
-                            className="mt-2 text-blue-500 underline text-lg "
-                        >
-                            อ่านต่อ
-                        </a>
-                        <div className="mt-2 flex flex-wrap gap-2 text-lg text-gray-600">
-                            <span className="">หมวด</span>
-                            <span className="underline">เกาะ</span>
-                            <span className="underline">ทะเล</span>
-                            <span className="underline">อุทยาน</span>
-                            <span className="underline">ธรรมชาติ</span>
-                            <span className="underline">ท่องเที่ยว</span>
-                        </div>
-                        <figure className="mt-3 flex gap-2">
-                            <img
-                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png"
-                                alt=""
-                                className="w-16 h-16 rounded-md  object-cover object-center "
-                            />
-                            <img
-                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png"
-                                alt=""
-                                className="w-16 h-16 rounded-md object-cover object-center "
-                            />
-                            <a
-                                href=""
-                                className="ml-auto w-8 h-8 p-2 rounded-full border-2 border-sky-400 text-sky-400 hover:border-sky-600  hover:bg-sky-100 cursor-pointer object-center flex justify-center items-center"
+            <section className="max-w-7xl w-[90%] mx-auto m-6 space-y-6 flex flex-col items-center overflow-hidden">
+                {dataServer ? (
+                    dataServer.map((dataTravel) => {
+                        return (
+                            <div
+                                className="p-4 bg-white rounded-lg shadow-md flex gap-4 "
+                                key={dataTravel.eid}
                             >
-                                🔗
-                            </a>
-                        </figure>
-                    </div>
-                </div>
+                                <img
+                                    src={dataTravel.photos[0]}
+                                    alt=""
+                                    className="w-1/3 object-cover object-center rounded-3xl"
+                                />
+                                <div className="flex-1 overflow-hidden ">
+                                    <h2 className="text-xl font-semibold  ">
+                                        {dataTravel.title}
+                                    </h2>
+                                    <p
+                                        className={`text-lg  mt-1 w-full text-gray-600  `}
+                                    >
+                                        {dataTravel.description}
+                                    </p>
+                                    <a
+                                        href={dataTravel.url}
+                                        className="mt-2 text-blue-500 underline text-lg "
+                                    >
+                                        อ่านต่อ
+                                    </a>
+                                    <div className="mt-2 flex flex-wrap gap-2 text-lg text-gray-600">
+                                        <span className="">หมวด</span>
+                                        {dataTravel.tags.map((tagTravel) => {
+                                            return (
+                                                <span className="underline">
+                                                    {tagTravel}
+                                                </span>
+                                            );
+                                        })}
+                                    </div>
+                                    <figure className="mt-3 flex gap-2">
+                                        {dataTravel.photos
+                                            .slice(1)
+                                            .map((photoTarvel) => {
+                                                return (
+                                                    <img
+                                                        src={photoTarvel}
+                                                        alt=""
+                                                        className="w-16 h-16 rounded-md  object-cover object-center "
+                                                    />
+                                                );
+                                            })}
+                                        <a
+                                            href={dataTravel.url}
+                                            className="ml-auto w-8 h-8 p-2 rounded-full border-2 border-sky-400 text-sky-400 hover:border-sky-600  hover:bg-sky-100 cursor-pointer object-center flex justify-center items-center"
+                                        >
+                                            🔗
+                                        </a>
+                                    </figure>
+                                </div>
+                            </div>
+                        );
+                    })
+                ) : (
+                    <h1>...londing</h1>
+                )}
             </section>
         </main>
     );
