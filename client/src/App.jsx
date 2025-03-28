0649c79b549531df0ac3a3ef9e2c17ccf8ef0dc3
Charlee2543 <165679195+Charlee2543@users.noreply.github.com>
2025-03-29 00:35:51 +0700

    feat:add open link in title, click tag to input ,copy URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,123 +18,136 @@ import { useState, useEffect } from 'react';
 fetch(`https://demo.dataverse.org/api/search?q=${value}`)
 */
 function App() {
-    const [dataServer, Setdata] = useState([]);
-    const [inputSearch, setInputSearch] = useState('');
+   const [dataServer, Setdata] = useState([]);
+   const [inputSearch, setInputSearch] = useState('');
 
-    const getData = async (keywords) => {
-        try {
-            const result = await axios.get(
-                `http://localhost:4001/trips?keywords=${
-                    keywords ? keywords : ' '
-                }`
-            );
-            console.log('result: ', result.data.data);
-            Setdata(result.data.data);
-            console.log('getData complete');
-        } catch (error) {
-            console.log('error getData: ', error);
-        }
-    };
+   const getData = async (keywords) => {
+      try {
+         const result = await axios.get(
+            `http://localhost:4001/trips?keywords=${keywords ? keywords : ' '}`
+         );
+         console.log('result: ', result.data.data);
+         Setdata(result.data.data);
+         console.log('getData complete');
+      } catch (error) {
+         console.log('error getData: ', error);
+      }
+   };
 
-    useEffect(() => {
-        getData();
-    }, []);
-    useEffect(
-        debounce(() => {
-            getData(inputSearch);
-        }, 1500),
-        [inputSearch]
-    );
-    console.log('inputSearch: ', inputSearch);
-    console.log('data: ', dataServer);
+   useEffect(() => {
+      getData();
+   }, []);
+   useEffect(
+      debounce(() => {
+         getData(inputSearch);
+      }, 1500),
+      [inputSearch]
+   );
+   console.log('inputSearch: ', inputSearch);
+   console.log('data: ', dataServer);
 
-    // const text =
-    //     'เริ่มเที่ยวเชิงอนุรักษ์แบบชิลล์ๆ กันที่แรก ที่ศูนย์อนุรักษ์ป่าชายเลน คลองโคน ด้วยกิจกรรมอัดแน่น ทั้งการล่องเรือชมป่าชายเลน รวมสร้างแหล่งอาหารให้สัตว์ทะเลด้วยการปลูกต้นแสม ต้นลำพู สัมผัสวิถีชีวิตชาวประมงในการเลี้ยงหอยนางรม และหอยแมลงภู่ แถมด้วยไฮไลท์น่ารักๆ นั่นก็คือ เจ้าลิงแสมที่ออกมาวิ่งเล่น ต้อนรับนักท่องเที่ยวชมความสมบูรณ์ของระบบนิเวศน์ที่นี่';
-
-    return (
-        <main className="App bg-gray-100 p-6 flex flex-col items-center  ">
-            <h1 className=" text-6xl text-sky-500 ">เที่ยวไหนดี</h1>
-            <label
-                htmlFor="searchTravel"
-                className="m-4  max-w-7xl w-[80%]  flex flex-col items-start gap-1   "
-            >
-                <h2 className="text-[1.5rem] font-normal ">ค้นหาที่เที่ยว</h2>
-                <input
-                    id="searchTravel"
-                    type="text"
-                    className="m-2 p-2 w-full text-[1.5rem] text-black border-b-1  border-gray-400 placeholder:text-gray-500 placeholder:text-center"
-                    placeholder="หาที่เที่ยวแล้วไปกัน"
-                    value={inputSearch}
-                    onChange={(event) => setInputSearch(event.target.value)}
-                />
-            </label>
-            <section className="max-w-7xl w-[90%] mx-auto m-6 space-y-6 flex flex-col items-center overflow-hidden">
-                {dataServer ? (
-                    dataServer.map((dataTravel) => {
-                        return (
-                            <div
-                                className="p-4 bg-white rounded-lg shadow-md flex gap-4 "
-                                key={dataTravel.eid}
-                            >
-                                <img
-                                    src={dataTravel.photos[0]}
+   return (
+      <main className="App bg-gray-100 p-6 flex flex-col items-center  ">
+         <h1 className=" text-6xl text-sky-500 ">เที่ยวไหนดี</h1>
+         <label
+            htmlFor="searchTravel"
+            className="m-4  max-w-7xl w-[80%]  flex flex-col items-start gap-1   "
+         >
+            <h2 className="text-[1.5rem] font-normal ">
+               ค้นหาที่เที่ยว แนะนำเลยมี {dataServer.length} ที่ 🛫
+            </h2>
+            <input
+               id="searchTravel"
+               type="text"
+               className="m-2 p-2 w-full text-[1.5rem] text-black border-b-1  border-gray-400 placeholder:text-gray-500 placeholder:text-center"
+               placeholder="หาที่เที่ยวแล้วไปกัน"
+               value={inputSearch}
+               onChange={(event) => setInputSearch(event.target.value)}
+            />
+         </label>
+         <section className="max-w-7xl w-[90%] mx-auto m-6 space-y-6 flex flex-col items-center ">
+            {dataServer.map((dataTravel) => {
+               return (
+                  <div
+                     className="p-4 bg-white rounded-lg shadow-md flex gap-4 overflow-hidden"
+                     key={dataTravel.eid}
+                  >
+                     <img
+                        src={dataTravel.photos[0]}
+                        alt=""
+                        className="w-[30%] object-cover object-center rounded-3xl"
+                     />
+                     <div className="flex-1 w-[70%]">
+                        <h2
+                           className="text-xl font-semibold cursor-pointer"
+                           onClick={() => {
+                              window.location.href = dataTravel.url;
+                           }}
+                        >
+                           {dataTravel.title}
+                        </h2>
+                        <p
+                           className={`text-lg max-w-full mt-1 w-full text-gray-600 overflow-hidden `}
+                        >
+                           {dataTravel.description.length > 100
+                              ? dataTravel.description.slice(0, 100) + '...'
+                              : dataTravel}
+                        </p>
+                        <a
+                           href={dataTravel.url}
+                           className="mt-2 text-blue-500 underline text-lg "
+                        >
+                           อ่านต่อ
+                        </a>
+                        <div className="mt-2 flex flex-wrap gap-2 text-lg text-gray-600">
+                           <span className="">หมวด</span>
+                           {dataTravel.tags.map((tagTravel) => {
+                              return (
+                                 <span
+                                    className="underline cursor-pointer"
+                                    onClick={() => {
+                                       setInputSearch((inputData) => {
+                                          let result = '';
+                                          inputData
+                                             ? (result =
+                                                  inputData + ' ' + tagTravel)
+                                             : (result = tagTravel);
+                                          return result;
+                                       });
+                                    }}
+                                 >
+                                    {tagTravel}
+                                 </span>
+                              );
+                           })}
+                        </div>
+                        <figure className="mt-3 flex gap-2">
+                           {dataTravel.photos.slice(1).map((photoTarvel) => {
+                              return (
+                                 <img
+                                    src={photoTarvel}
                                     alt=""
-                                    className="w-1/3 object-cover object-center rounded-3xl"
-                                />
-                                <div className="flex-1 overflow-hidden ">
-                                    <h2 className="text-xl font-semibold  ">
-                                        {dataTravel.title}
-                                    </h2>
-                                    <p
-                                        className={`text-lg  mt-1 w-full text-gray-600  `}
-                                    >
-                                        {dataTravel.description}
-                                    </p>
-                                    <a
-                                        href={dataTravel.url}
-                                        className="mt-2 text-blue-500 underline text-lg "
-                                    >
-                                        อ่านต่อ
-                                    </a>
-                                    <div className="mt-2 flex flex-wrap gap-2 text-lg text-gray-600">
-                                        <span className="">หมวด</span>
-                                        {dataTravel.tags.map((tagTravel) => {
-                                            return (
-                                                <span className="underline">
-                                                    {tagTravel}
-                                                </span>
-                                            );
-                                        })}
-                                    </div>
-                                    <figure className="mt-3 flex gap-2">
-                                        {dataTravel.photos
-                                            .slice(1)
-                                            .map((photoTarvel) => {
-                                                return (
-                                                    <img
-                                                        src={photoTarvel}
-                                                        alt=""
-                                                        className="w-16 h-16 rounded-md  object-cover object-center "
-                                                    />
-                                                );
-                                            })}
-                                        <a
-                                            href={dataTravel.url}
-                                            className="ml-auto w-8 h-8 p-2 rounded-full border-2 border-sky-400 text-sky-400 hover:border-sky-600  hover:bg-sky-100 cursor-pointer object-center flex justify-center items-center"
-                                        >
-                                            🔗
-                                        </a>
-                                    </figure>
-                                </div>
-                            </div>
-                        );
-                    })
-                ) : (
-                    <h1>...londing</h1>
-                )}
-            </section>
-        </main>
-    );
+                                    className="w-16 h-16 rounded-md  object-cover object-center "
+                                 />
+                              );
+                           })}
+                           <a
+                              // href={dataTravel.url}
+                              className="ml-auto w-8 h-8 p-2 rounded-full border-2 border-sky-400 text-sky-400 hover:border-sky-600  hover:bg-sky-100 cursor-pointer object-center flex justify-center items-center"
+                              onClick={async () => {
+                                 navigator.clipboard.writeText(dataTravel.url);
+                              }}
+                           >
+                              🔗
+                           </a>
+                        </figure>
+                     </div>
+                  </div>
+               );
+            })}
+         </section>
+      </main>
+   );
 }
 
 export default App;
